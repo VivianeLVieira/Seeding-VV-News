8b58dc3c124686f2ff57f4a4b21a136f11ac7b7f
Viviane Vieira <vivianevieira@Vivianes-MacBook-Pro.local>
2025-04-16 22:16:36 +0100

    Task 4 from Seeding NC News

--- db/selects.js
+++ db/selects.js
@@ -1,6 +1,38 @@
 const db = require("./connection")
 
 
-db.query('SELECT * FROM users').then((results) =>{
+function getAllUsers(){ // Get all of the users
+    db.query('SELECT * FROM users;').then((results) =>{ 
+        console.log(results);
+    })
+}
+//getAllUsers();
+
+/* //Get all of the articles where the topic is coding
+db.query(`SELECT * FROM articles WHERE articles.topic = 'coding';`).then((results) =>{
+    console.log(results);
+})
+*/
+
+/*//Get all of the comments where the votes are less than zero
+db.query(`SELECT * FROM comments WHERE votes < 0;`).then((results) =>{
+    console.log(results);
+})
+*/
+
+/*//Get all of the topics
+db.query(`SELECT * FROM topics;`).then((results) =>{
+    console.log(results);
+})
+*/    
+
+/*//Get all of the articles by user grumpy19
+db.query(`SELECT * FROM articles WHERE author = 'grumpy19';`).then((results) =>{
+    console.log(results);
+})
+*/
+
+//Get all of the comments that have more than 10 votes
+db.query(`SELECT * FROM comments WHERE votes > 10;`).then((results) =>{
     console.log(results);
 })
