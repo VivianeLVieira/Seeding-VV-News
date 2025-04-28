99f7fb9c9ff304eef34800073ad70de3fd552cc9
Viviane Vieira <vivianevieira@Vivianes-MacBook-Pro.local>
2025-04-28 12:50:42 +0100

    This commit will add endpoint CORE: GET /api

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,24 +2,25 @@ const endpointsJson = require("../endpoints.json");
 const db = require("../db/connection")
 const seed = require("../db/seeds/seed")
 const data = require("../db/data/test-data")
-//const app = require("../db/app.js");
+const app = require("../db/app")
 const request = require("supertest")
 
-beforeAll(() => seed(data));
-afterAll(() => db.end());
+beforeAll(() => {
+  return seed(data)
+});
 
+afterAll(() => {
+  return db.end()
+});
 
-/* Set up your test imports here */
-
-/* Set up your beforeEach & afterAll functions here */
-
-describe.skip("GET /api", () => {
+describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
     return request(app)
       .get("/api")
       .expect(200)
-      .then(({ body: { endpoints } }) => {
+      .then(({ body: { endpoints }}) => {
         expect(endpoints).toEqual(endpointsJson);
+        expect(typeof endpoints).toBe("object");
       });
   });
 });
